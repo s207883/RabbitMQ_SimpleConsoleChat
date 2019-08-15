b1b3ab10c2de6b2df5d6b0c70c0dbafcd0fa56e7
Горелкин Александр <s207883@yandex.ru>
2019-08-15 13:27:58 +0300

    Добавлены ссылки в ConsoleApp на DLL и DAL

---
+++ SimpleChat.BLL/Implementations/MessageController.cs
@@ -0,0 +1,32 @@
+﻿using SimpleChat.BLL.Interfaces;
+using SimpleChat.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChat.BLL.Implementations
+{
+	class MessageController : IMessageController
+	{
+		public Task<List<Message>> ReceiveMessagesAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task ReConfigureControllerAsync(string host, string port, string virtualHost, string userName, string userPassword)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SendMessageAsync(Message message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SendMessagesAsync(IList<Message> messages)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
