8703accf122c7fd1dfbf3edf4b96833334cf35b3
Горелкин Александр <s207883@yandex.ru>
2019-08-15 13:25:07 +0300

    Добавлена модель сообщения. Добавлен интерфейс контроллера сообщений.

---
+++ SimpleChat.BLL/Interfaces/IMessageController.cs
@@ -0,0 +1,16 @@
+﻿using SimpleChat.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChat.BLL.Interfaces
+{
+	public interface IMessageController
+	{
+		Task ReConfigureControllerAsync(string host, string port, string virtualHost, string userName, string userPassword);
+		Task SendMessageAsync(Message message);
+		Task SendMessagesAsync(IList<Message> messages);
+		Task<List<Message>> ReceiveMessagesAsync();
+	}
+}
---
+++ SimpleChat.DAL/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleChat.DAL.Models
+{
+	public class Message
+	{
+	}
+}
